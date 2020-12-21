2f84cb4ff3cbe7be507176d3bea8ab297bb2a1e3
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-21 12:48:20 +0400

    Finish project

--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@ using namespace std;
 int main()
 {
     cout << "Press Ctrl-C to stop" << endl;
-    int lenghtOfArray;
-    cout << "Enter the number of numbers you want to enter: ";
-    cin >> lenghtOfArray;
-    int a[lenghtOfArray];
-    for (int i = 0; i < lenghtOfArray; i++)
+    cout << "You will enter 3 numbers" << endl;
+    int a[3];
+    for (int i = 0; i < 3; i++)
     {
         cout << "Enter a number: ";
         cin >> a[i];
     }
     cout << "This is what you've entered:" << endl;
-    cout << a;
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << a[i] << " ";
+    }
 
     return 0;
 }
