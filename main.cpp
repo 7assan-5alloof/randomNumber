b8fb94371099896ddd882d6bc021db8aa52e47dc
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-21 12:40:15 +0400

    Write part of the program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Press Ctrl-C to stop" << endl;
+    int lenghtOfArray;
+    cout << "Enter the number of numbers you want to enter: ";
+    cin >> lenghtOfArray;
+    int a[lenghtOfArray];
+    for (int i = 0; i < lenghtOfArray; i++)
+    {
+        cout << "Enter a number: ";
+        cin >> a[i];
+    }
+    cout << "This is what you've entered:" << endl;
+    cout << a;
+
     return 0;
 }
